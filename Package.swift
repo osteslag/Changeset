2aa6372ad8df8bf05474c40d3a0fca89a62c763e
Dave Anderson <david.anderson@robotsandpencils.com>
2019-06-14 15:48:27 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,10 +1,39 @@
-//
-//  Package.swift
-//  Changeset
-//
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-	name: "Changeset"
+    name: "Changeset",
+    platforms: [
+        .macOS(.v10_10),
+        .iOS(.v8),
+        .tvOS(.v9),
+        .watchOS(.v2)
+        // Note: These might not be accurate
+    ],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "Changeset",
+            targets: ["Changeset"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "Changeset",
+            dependencies: [],
+            path: "Sources"
+        ),
+        .testTarget(
+            name: "ChangesetTests",
+            dependencies: ["Changeset"],
+            path: "Tests"
+        ),
+    ]
 )
